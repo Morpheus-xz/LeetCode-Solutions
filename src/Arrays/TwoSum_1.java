663bee9da3ab88a9397038730f89df36d74a3d74
Vedansh-Agarwal-2007 <vedanshagarwaldpr1234@gmail.com>
2025-09-28 14:19:38 +0530

    Added 1. Two Sum Solution

--- src/Arrays/TwoSum_1.java
+++ src/Arrays/TwoSum_1.java
@@ -1,5 +1,17 @@
-package Arrays;
-
-public class TwoSum_1 {
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        HashMap <Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int rem = target-nums[i];
+            if(map.containsKey(rem)){
+                ans[0]=i;
+                ans[1]=map.get(rem);
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return ans;
+    }
 }

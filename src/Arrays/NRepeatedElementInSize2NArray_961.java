8264f0bb6ea3543f2f0ffc79ffdabd5ca14b68fc
Vedansh-Agarwal-2007 <vedanshagarwaldpr1234@gmail.com>
2026-01-02 13:16:16 +0530

    Added Leetcode 961 solution

---
+++ src/Arrays/NRepeatedElementInSize2NArray_961.java
@@ -0,0 +1,17 @@
+class Solution{
+    public int repeatedNTimes(int[] nums) {
+        Arrays.sort(nums);
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count == nums.length / 2) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
